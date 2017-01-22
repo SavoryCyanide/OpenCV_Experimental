7a2cdccb86c6e3db0e9122fd2b90959d291d593b
Kenny <savorycyanide@gmail.com>
2017-01-22 02:17:46 +0000

    Issue: Getting same frame

--- 2.subtraction/subtraction.cpp
+++ 2.subtraction/subtraction.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cv.h>
 #include <opencv2/highgui/highgui.hpp>
@@ -10,39 +11,41 @@ int main()
 {
     String pointless;
     Mat frame1, frame2, subtracted, cvtGray;
+
     VideoCapture cap(0);
+    double fps = cap.get(CV_CAP_PROP_FPS);
+    std::cout << "Fps = " << fps << std::endl;
+
     std::cout << "Initialized\n";
 
-    //Capture and display unlit frame
+//Capture and display unlit frame
     cap >> frame1;
-    //namedWindow("Frame1", CV_WINDOW_AUTOSIZE);
     imwrite("frame1.jpg", frame1);
-    //imshow("Frame1", frame1);
     std::cout << "Frame1\n";
 
-    //Figure out how to get gpio working
+
+//Figure out how to get gpio working
     std::cin >> pointless; 
 
-    //Capture and display lit frame
+//Capture and display lit frame
     cap >> frame2;
-    //namedWindow("Frame2", CV_WINDOW_AUTOSIZE);
+
     imwrite("frame2.jpg", frame2);
-    //imshow("Frame2", frame2);
-    std::cout << "Frame2";
 
-    //Process images and display results
+    std::cout << "Frame2\n";
+
+//Process images and display results
     subtract(frame1, frame2, subtracted);
-    //namedWindow("Subtracted", CV_WINDOW_AUTOSIZE);
     imwrite("subtracted.jpg", subtracted);
-    //imshow("Subtracted", subtracted);
-    std::cout << "Subtracted";
+    std::cout << "Subtracted\n";
 
-    //Convert subtracted image to gray (and blur)?
+//Convert subtracted image to gray (and blur)?
         //cvtColor(sourceImage, destination, option)
     cvtColor(subtracted, cvtGray, CV_BGR2GRAY);
     blur(cvtGray, cvtGray, Size(3,3));
     imwrite("gray/blur.jpg", cvtGray);
-    std::cout << "Gray and blur";
+    std::cout << "Gray and blur\n";
+
 
     return 0;
 }
