ae27c0b0da7ccf44019cdb5364651088eff8abf9
zeli22 <zeli2@s.sfusd.edu>
2017-02-19 02:46:27 +0000

    video working

--- 3.video/build/CMakeFiles/cv_video.dir/DependInfo.cmake
+++ 3.video/build/CMakeFiles/cv_video.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ubuntu/Desktop/vis2017/testing_area/openCV_Experimental/3.video/video.cpp" "/home/ubuntu/Desktop/vis2017/testing_area/openCV_Experimental/3.video/build/CMakeFiles/cv_video.dir/video.cpp.o"
+  "/home/ubuntu/Desktop/forpush/openCV_Experimental/3.video/video.cpp" "/home/ubuntu/Desktop/forpush/openCV_Experimental/3.video/build/CMakeFiles/cv_video.dir/video.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- 3.video/video.cpp
+++ 3.video/video.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-int main() {
-    cv::namedWindow( "Example3", cv::WINDOW_AUTOSIZE );
-    cv::VideoCapture cap(0);
-    cv::Mat frame;
-
-    if(!videoCapture->isOpened())
-        throw Exception();
-
-    while( 1 )
+#include "opencv2/opencv.hpp"
+using namespace cv;
+int main(int argc, char** argv)
+{
+    VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if(!cap.open(0))
+        return 0;
+    for(;;)
     {
-        cap >> frame;
-        cv::imshow( "Example3", frame );
+          Mat frame;
+          cap >> frame;
+          if( frame.empty() ) break; // end of video stream
+          imshow("this is you, smile! :)", frame);
+          if( waitKey(1) == 27 ) break; // stop capturing by pressing ESC 
     }
-
+    // the camera will be closed automatically upon exit
+    // cap.close();
     return 0;
 }
