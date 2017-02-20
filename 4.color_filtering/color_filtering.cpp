e262a51b5b77f64ee09af11ad649e2c56b4a128d
Kenny Ngo <savorycyanide@gmail.com>
2017-02-19 23:13:37 -0800

    added 4.color_filtering

---
+++ 4.color_filtering/color_filtering.cpp
@@ -0,0 +1,36 @@
+#include "opencv2/opencv.hpp"
+using namespace cv;
+int main(int argc, char** argv)
+{
+    VideoCapture cap;
+
+    if(!cap.open(0))
+        return 0;
+    for(;;)
+    {
+        Mat frame;
+
+        cap >> frame;
+        if( frame.empty() ) 
+            break; // end of video stream
+
+        Mat onlyGreen = greenFilter(frame);
+
+        imshow("Only green", onlyGreen);
+
+        if( waitKey(1) == 27 ) 
+            break; // stop capturing by pressing ESC 
+    }
+    // cap.close();
+    return 0;
+}
+
+Mat greenFilter(const Mat& src)
+{
+    assert(src.type() == CV_8UC3);
+
+    Mat greenOnly;
+    inRange(src, Scalar(0, 0, 0), Scalar(0, 255, 0), greenOnly);
+
+    return redOnly;
+}
