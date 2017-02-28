6b8649d42ae0df4f9e99d30230de4a9367a8ff49
Kenny Ngo <savorycyanide@gmail.com>
2017-02-27 16:34:37 -0800

    Added bounding box and corner detection

--- 8.bounding_box_&_corner/hull.cpp
+++ 8.bounding_box_&_corner/hull.cpp
@@ -9,6 +9,7 @@ using namespace std;
 Mat blur(const Mat& src);
 Mat colorFilter(const Mat& src, int maxH, int maxS, int maxV, int minH, int minS, int minV);
 Mat contour(const Mat& src, int thresh);
+Mat corners(const Mat& src, int thresh);
 
 
 int main(int argc, char** argv)
@@ -20,7 +21,9 @@ int main(int argc, char** argv)
 
     //Create trackbar
     namedWindow("Color Filtered", 1);
+    namedWindow("Blurred", 1);
     namedWindow("Contour", 1);
+    namedWindow("Corner", 1);
 
     //Starting HSV values
     int minH = 44;
@@ -31,6 +34,8 @@ int main(int argc, char** argv)
     int maxS = 67;
     int maxV = 255;
 
+    int cornerThresh = 235;
+
     createTrackbar("Min H", "Color Filtered", &minH, 179);
     createTrackbar("Max H", "Color Filtered", &maxH, 179);
     
@@ -39,6 +44,8 @@ int main(int argc, char** argv)
 
     createTrackbar("Min V", "Color Filtered", &minV, 255);
     createTrackbar("Max V", "Color Filtered", &maxV, 255);
+
+    createTrackbar("Thresh", "Corner", &cornerThresh, 255);
 
     int contour_thresh = 450;
     createTrackbar("Contour Thresh", "Contour", &contour_thresh, 500);
@@ -49,27 +56,31 @@ int main(int argc, char** argv)
 
     for(;;)
     {
-        Mat frame, blurred, filtered, contoured;
+        Mat frame, blurred, filtered, contoured, corner;
 
         if (videoBool == true)
             cap >> frame;
         else
             frame = imread(loadPic, 1);
 
-        if( frame.empty() ) 
+        if (frame.empty())
+        {
+            cout << "Photo \"" + loadPic + "\" not found, exiting\n";
             break; // end of video stream
-
+        }
 
         //PROCESSING/////////////////////////
         blurred = blur(frame);
         filtered = colorFilter(blurred, maxH, maxS, maxV, minH, minS, minV);
         contoured = contour(filtered, contour_thresh);
+        corner = corners(contoured, cornerThresh);
         ////////////////////////////////////
 
         imshow("Regular", frame);
         //imshow("Blurred", blurred);
         imshow("Color Filtered", filtered);
         imshow("Contour", contoured);
+        imshow("Corner", corner);
 
         if( waitKey(1) == 32 )
         {
@@ -128,21 +139,73 @@ Mat contour(const Mat& src, int thresh)
     /// Find contours
     findContours( canny_output, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
 
-    //Convex hull
-    vector<vector<Point> >hull( contours.size() );
-    for( int i = 0; i < contours.size(); i++ )
-    {  convexHull( Mat(contours[i]), hull[i], true ); }
-
-    // Draw
+    /// Draw contours
     Mat drawing = Mat::zeros(src.rows, src.cols, CV_8UC3);
 
     for( int i = 0; i< contours.size(); i++ )
     {
         Scalar color = Scalar(255, 255, 255);
         //drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-        drawContours( drawing, hull, i, Scalar(0,255,0), 1, 8, vector<Vec4i>(), 0, Point() );
+    }
+//
+
+    Mat threshold_output;
+    threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
+
+    vector<vector<Point> > contours_poly( contours.size() );
+    vector<Rect> boundRect( contours.size() );
+    //vector<Point2f>center( contours.size() );
+    //vector<float>radius( contours.size() );
+    for( size_t i = 0; i < contours.size(); i++ )
+    {
+      approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+      boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+      //minEnclosingCircle( contours_poly[i], center[i], radius[i] );
+    }
+
+    for( size_t i = 0; i< contours.size(); i++ )
+    {
+      Scalar color = Scalar( 0, 255, 0 );
+      //drawContours( drawing, contours_poly, (int)i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+      rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+      //circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
     }
 
     return drawing;
 }
 
+
+Mat corners(const Mat& source, int thresh)
+{
+  Mat src;
+
+  cvtColor(source, src, CV_RGB2GRAY);
+
+  Mat dst, dst_norm, dst_norm_scaled;
+  dst = Mat::zeros( src.size(), CV_32FC1 );
+
+  /// Detector parameters
+  int blockSize = 2;
+  int apertureSize = 3;
+  double k = 0.04;
+
+  /// Detecting corners
+  cornerHarris( src, dst, blockSize, apertureSize, k, BORDER_DEFAULT );
+
+  /// Normalizing
+  normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
+  convertScaleAbs( dst_norm, dst_norm_scaled );
+
+  /// Drawing a circle around corners
+  for( int j = 0; j < dst_norm.rows ; j++ )
+     { for( int i = 0; i < dst_norm.cols; i++ )
+          {
+            if( (int) dst_norm.at<float>(j,i) > thresh )
+              {
+               circle( dst_norm_scaled, Point( i, j ), 5,  Scalar(0), 2, 8, 0 );
+              }
+          }
+     }
+
+  return dst_norm_scaled;
+}
